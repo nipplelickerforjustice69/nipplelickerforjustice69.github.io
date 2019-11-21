bd841c73cabdd232aae1cf56642d85229dbd584b
rizal_darmawan <38039365+nipplelickerforjustice69@users.noreply.github.com>
2019-11-21 11:24:33 +0700

    commit 1

---
+++ precache-manifest.d8a6d5666516a753dcea9658766529d1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b36ee3b9ae82b5056f",
+    "url": "/css/about.3b3c9ddb.css"
+  },
+  {
+    "revision": "73dfd09e87096a3660f6",
+    "url": "/css/chunk-vendors.ec4de8ca.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "de87a2e8a31be364df34677a889c96a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60b36ee3b9ae82b5056f",
+    "url": "/js/about.9ed4a99b.js"
+  },
+  {
+    "revision": "a36b8ec1b4f56acda4c8",
+    "url": "/js/app.e7bacdfc.js"
+  },
+  {
+    "revision": "73dfd09e87096a3660f6",
+    "url": "/js/chunk-vendors.11360622.js"
+  },
+  {
+    "revision": "2a7ab406a30b5bd326cd0d0fd1e55d11",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
